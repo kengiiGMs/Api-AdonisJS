8fe960660df548fe45baa95a54e548436ffbf01a
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-13 09:34:45 -0300

    feat(controllers): Controller de validação e remoção do token do diretor

--- start/routes/diretores.ts
+++ start/routes/diretores.ts
@@ -2,3 +2,8 @@ import Route from "@ioc:Adonis/Core/Route";
 
 Route.post('/diretores', 'Diretores/CadastrarDiretoresController')
 Route.post('/diretores/login', 'Diretores/LoginDiretoresController')
+
+Route.group(() => {
+    Route.post('/diretores/token/validar', 'Diretores/ValidarTokenDiretoresController')
+    Route.post('/diretores/token/remover', 'Diretores/RemoverTokenDiretoresController')
+}).middleware('auth:diretor')
