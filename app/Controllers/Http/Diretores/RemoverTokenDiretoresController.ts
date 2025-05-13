93cd3b9f304b6caef569008c073e293cc4d915cb
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-13 09:47:21 -0300

    refactor(controllers): Remoção de atributos e estruturas não utilizadas

--- app/Controllers/Http/Diretores/RemoverTokenDiretoresController.ts
+++ app/Controllers/Http/Diretores/RemoverTokenDiretoresController.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class RemoverTokenDiretoresController {
     public async handle({ request, response, auth }: HttpContextContract) {
-        const remover = await auth.use('diretor').revoke()
-        return response.json({ message: "Deslogado", remover })
+        await auth.use('diretor').revoke()
+        return response.json({ message: "Deslogado" })
     }
 }
