9960fdda9c6a7301f420133951d157fa95e14dcc
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-14 18:01:15 -0300

    feat(estudantes/professores): Aplicando rotas privadas em estudantes e professores

--- start/routes/estudantes.ts
+++ start/routes/estudantes.ts
@@ -6,10 +6,10 @@ Route.post('/estudantes/login', 'Estudantes/LoginEstudantesController')
 Route.group(() => {
     Route.get('/estudantes', 'Estudantes/ListarEstudantesController')
     Route.delete('/estudantes/:estudante_id', 'Estudantes/DeletarEstudantesController')
-    Route.put('/estudantes/:estudante_id', 'Estudantes/EditarEstudantesController')
-}).middleware('auth:diretor')
+}).middleware('multiAuthProfessorDiretor')
 
 Route.group(() => {
     Route.post('/estudantes/token/validar', 'Estudantes/ValidarTokenEstudantesController')
     Route.post('/estudantes/token/remover', 'Estudantes/RemoverTokenEstudantesController')
+    Route.put('/estudantes/:estudante_id', 'Estudantes/EditarEstudantesController')
 }).middleware('auth:estudante')
--- start/routes/professores.ts
+++ start/routes/professores.ts
@@ -1,7 +1,10 @@
 import Route from "@ioc:Adonis/Core/Route";
 
-Route.post('/professores', 'Professores/CadastrarProfessoresController')
 Route.post('/professores/login', 'Professores/LoginProfessoresController')
+
+Route.group(() => {
+    Route.post('/professores', 'Professores/CadastrarProfessoresController')
+}).middleware('auth:diretor')
 
 Route.group(() => {
     Route.post('/professores/token/validar', 'Professores/ValidarTokenProfessoresController')
